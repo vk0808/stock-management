d03d566796f5c93ae8e936138a07121058847bb0
vk0808 <vk4765712@gmail.com>
2021-08-14 13:19:42 +0530

    feat: stock class

---
+++ StockManagement/Stock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockManagement
+{
+    public class Stock
+    {
+        public string _name;
+        public double _noOfShares;
+        public double _sharePrice;
+
+        public Stock(string name, double noOfShares, double sharePrice)
+        {
+            this._name = name;
+            this._noOfShares = noOfShares;
+            this._sharePrice = sharePrice;
+        }
+    }
+}
