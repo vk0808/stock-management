8bb1e1f12b447d0f5432a6eb50ab6e0f19c3e168
vk0808 <vk4765712@gmail.com>
2021-08-14 12:58:12 +0530

    feat: welcome message

---
+++ StockManagement/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StockManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StockAccount stock = new StockAccount();
+            Console.WriteLine(stock.welcome());
+        }
+    }
+}
---
+++ StockManagement/StockAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockManagement
+{
+    public class StockAccount
+    {
+        public string welcome()
+        {
+            return "Welcome to Stock Account Management Program\n";
+        }
+    }
+}
